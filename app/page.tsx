79d3254436e10699a515cbc5fad0ccc1f5fde5d3
Fashkan25 <nartaevnafariza@gmail.com>
2025-11-22 16:56:23 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,15 @@
-
+export default function Page() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "32px",
+      }}
+    >
+      <h1 className="fade-in">Zertteu MATH Lab — жұмыс істеп тұр! 🚀</h1>
+    </main>
+  );
+}
